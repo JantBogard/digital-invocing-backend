4a6a813217d662b5ad58833e9ce313fc436fb424
JantBogard <joancenkonga@gmail.com>
2023-12-14 10:59:56 +0100

    Add swagger documentation

--- src/main/java/cm/uni2grow/digitalInvocing/manageAddress/model/dto/AddressDto.java
+++ src/main/java/cm/uni2grow/digitalInvocing/manageAddress/model/dto/AddressDto.java
@@ -1,7 +1,5 @@
 package cm.uni2grow.digitalInvocing.manageAddress.model.dto;
 
-import cm.uni2grow.digitalInvocing.manageCustomer.models.dao.Customer;
-import cm.uni2grow.digitalInvocing.manageInvoice.models.dao.Invoice;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +14,4 @@ public class AddressDto {
     private String state;
     private String zipCode;
     private String country;
-    private Invoice invoice;
-    private Customer customer;
 }
